b207f7dadf4a3b1e7237c4e78295f71deac2f70c
queenk <qiukang@mail.ustc.edu.cn>
2017-10-01 23:57:39 +0800

    Initial commit

---
+++ src/main/java/cn/mageek/MageekcmsApplication.java
@@ -0,0 +1,19 @@
+package cn.mageek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+//暂时不配置数据源 所以必须exclude上面这两个
+//下面这个是为了暂时关闭security
+//也可以在 application.properties 中
+// security.basic.enabled=false
+// management.security.enabled=false
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class})
+public class MageekcmsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MageekcmsApplication.class, args);
+	}
+}
