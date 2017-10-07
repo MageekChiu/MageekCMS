ddcb5d9195b8d8d873755dd450912d0e8720b89d
queenk <qiukang@mail.ustc.edu.cn>
2017-10-07 23:25:30 +0800

    https http 双通道，且各自session不同，切换需要重新登陆

---
+++ src/main/java/cn/mageek/config/HttpsConfiguration.java
@@ -0,0 +1,37 @@
+package cn.mageek.config;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+@Configuration
+public class HttpsConfiguration {
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() throws IOException{
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+        tomcat.addAdditionalTomcatConnectors(httpConnector());
+        return tomcat;
+    }
+
+    private Connector httpConnector() throws IOException{
+        Connector connector = new Connector(Http11NioProtocol.class.getName());
+        Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+        connector.setPort(8999);
+        connector.setSecure(true);
+        connector.setScheme("https");
+        protocol.setSSLEnabled(true);
+        protocol.setKeyAlias("MageekPM");
+        protocol.setKeystorePass("1234567");
+        protocol.setKeyPass("1234567");
+        protocol.setKeystoreFile(new ClassPathResource("tomcat.keystore").getFile().getAbsolutePath());
+        protocol.setSslProtocol("TLS");//注意大小写
+        return connector;
+    }
+}
