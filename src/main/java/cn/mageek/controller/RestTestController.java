aaa9bc209e2fcefc0cacfb43235d61a01919bc3b
queenk <qiukang@mail.ustc.edu.cn>
2017-10-08 15:47:43 +0800

    应用层缓存

--- src/main/java/cn/mageek/controller/RestTestController.java
+++ src/main/java/cn/mageek/controller/RestTestController.java
@@ -1,6 +1,7 @@
 package cn.mageek.controller;
 
 import cn.mageek.pojo.Form;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,17 +10,27 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 public class RestTestController {
 
+    private static AtomicInteger visit = new AtomicInteger(0);
     @RequestMapping("/rest")
+    @Cacheable("searches")
     public Form hello(){
         Form form = new Form();
         form.setName("jobs");
         form.setEmail("12121@qq.com");
         form.setHobbies(Arrays.asList("特殊", "212", "奥迪"));
         form.setBirthDate(LocalDate.now());
+        try {
+            Thread.sleep(5000);//模拟耗时操作，只有第一次才会等5秒以上才有结果，后面都是马上就有了，说明缓存起作用了
+            System.out.println(visit+" 次访问");
+            visit.addAndGet(1);//加一
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return form;
     }
 
