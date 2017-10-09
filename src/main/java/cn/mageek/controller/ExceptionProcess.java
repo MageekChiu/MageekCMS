51b930ceea6bfe93edcac93000de8e2aedc4e21b
queenk <qiukang@mail.ustc.edu.cn>
2017-10-09 15:15:36 +0800

    全局ModelAttribute

--- src/main/java/cn/mageek/controller/ExceptionProcess.java
+++ src/main/java/cn/mageek/controller/ExceptionProcess.java
@@ -1,7 +1,9 @@
 package cn.mageek.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.util.WebUtils;
@@ -21,6 +23,11 @@ public class ExceptionProcess {
 //        return modelAndView;
 //    }
 
+    @ModelAttribute
+    public void addAttr(Model model){
+        model.addAttribute("msg","全局可见，所有标记了requestmapping的方法都可以获取");
+    }
 
+    
 
 }
